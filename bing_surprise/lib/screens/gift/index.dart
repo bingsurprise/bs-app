526cdf415c76edc402f6219eab74444ebd60392b
Yucong Jin <yucongj@microsoft.com>
2019-07-21 16:03:59 +0800

    update gift appbar

--- bing_surprise/lib/screens/gift/index.dart
+++ bing_surprise/lib/screens/gift/index.dart
@@ -3,7 +3,14 @@ import 'package:flutter/material.dart';
 class GiftIndex extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text('TODO...');
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('挑选礼物'),
+        leading: IconButton(icon:Icon(Icons.arrow_back),
+          onPressed:() => Navigator.pop(context, false),
+        )
+      ),
+    );
   }
 
 }
